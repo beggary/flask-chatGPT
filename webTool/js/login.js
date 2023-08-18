2d9ec74b42d3139b526e6ab64fdf6981103cc25d
beggary <68416662+beggary@users.noreply.github.com>
2023-08-18 17:08:14 +0800

    Add files via upload
    第一次上传

---
+++ webTool/js/login.js
@@ -0,0 +1,25 @@
+function login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    if (username === "") {
+        alert("请输入内容！");
+    } else {
+        $.ajax({
+            type: "POST",
+            url: "/login_in",
+            data: {"username": username, "password": password},
+            success: function (response) {
+                if (response.message === "ok") {
+                    window.location = "index";
+                } else if (response.message === "fail") {
+                    alert("用户名或密码错误！")
+                }
+            },
+            error: function (jqXHR) {
+                alert("未知错误！" + jqXHR.status)
+            },
+            complete: function () {
+            }
+        });
+    }
+}
---
+++ webTool/test.py
@@ -0,0 +1,5 @@
+{"role": "user", "content": "1"}, {"role": "assistant",
+                                   "content": "9191There are 91 days in the first quarter of a year.1There is 1 day in the first quarter of a year if we are considering the first day of the quarter."}, {
+    "role": "user", "content": "1"}, {"role": "assistant",
+                                      "content": "9191There are 91 days in the first quarter of a year.19191There are 91 days in the first quarter of a year.1There is 1 day in the first quarter of a year if we are considering the first day of the quarter.191"}, {
+    "role": "user", "content": "1"}
